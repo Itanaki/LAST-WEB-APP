c2ab88c4607a86a5c193916ccb6199436aa6d865
Itanaki <qedbpadual@tip.edu.ph>
2024-12-04 20:41:20 +0800

    Documents path

--- fish-silage-monitor/src/App.js
+++ fish-silage-monitor/src/App.js
@@ -1,7 +1,7 @@
   // src/App.js
   import React from 'react';
   import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-  import LandingPage from 'C:/Users/enzop/Documents/GitHub/LAST-WEB-APP/fish-silage-monitor/src/components/LandingPage/LandingPage.js'; // Correct path
+  import LandingPage from './components/LandingPage/LandingPage.js'; // Correct path
   import MonitoringPage from './components/MonitoringPage/MonitoringPage.js';
 
   const App = () => {
